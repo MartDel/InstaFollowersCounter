e42b6178553234430b8b4affa4ae86f924c15502
Martin Delebecque <martin-delebecque@outlook.fr>
2021-02-12 18:16:02 +0100

    Reorde files

--- digits.ino
+++ digits.ino
@@ -1,37 +1,3 @@
-#define NBDIGITS 8
-#define NBDISPLAY 5
-
-#define A 2
-#define B 3
-#define C 4
-#define D 5
-#define E 6
-#define G 7
-#define H 8
-#define FF 9
-
-const int nb_pins[5] = {A1, A2, A3, A4, A5};
-const int letter_pins[NBDIGITS] = {A, B, C, D, E, G, H, FF};
-
-void setup() {
-  for(int i = 0; i < NBDISPLAY; i++){
-    pinMode(nb_pins[i], OUTPUT);
-    digitalWrite(nb_pins[i], 1);
-  }
-  for(int i = 0; i < NBDIGITS; i++){
-    pinMode(letter_pins[i], OUTPUT);
-  }
-  
-  // Test first display
-  /*for(int i = 0; i < 10; i++){
-    setDigit(1, i);
-    delay(1000);
-  }*/
-}
-void loop() {
-  printNumber(12345);
-}
-
 // On/Off letter pins to print a number to the current digit
 void setNumber(int n) {
   bool combi[NBDIGITS];
@@ -68,9 +34,11 @@ void setDigit(int digit, int n) {
     digitalWrite(nb_pins[i], combi[i]);
     setNumber(n);
   }
-  if(digit == 2) digitalWrite(FF, 1);
-  else digitalWrite(FF, 0);
+  if(digit == 2) digitalWrite(DP, 1);
+  else digitalWrite(DP, 0);
 }
+
+// Print a number to digits displays
 void printNumber(int n) {
   int dm = n/10000;
   int m = (n-dm*10000)/1000;
@@ -84,22 +52,3 @@ void printNumber(int n) {
     delay(2);
   }
 }
-
-// Get digit sequence to print a specific number
-void setArrayContent(bool* array, bool n1, bool n2, bool n3, bool n4, bool n5, bool n6, bool n7, bool n8){
-  bool sequence[NBDIGITS] = {n1,n2,n3,n4,n5,n6,n7,n8};
-  for (size_t i = 0; i < NBDIGITS; i++) {
-    *array = sequence[i];
-    array++;
-  }
-}
-
-// Get digit sequence to print a specific number
-void setSmallArrayContent(bool* array, bool n1, bool n2, bool n3, bool n4, bool n5){
-  bool sequence[NBDISPLAY] = {n1,n2,n3,n4,n5};
-  for (size_t i = 0; i < NBDISPLAY; i++) {
-    *array = sequence[i];
-    array++;
-  }
-}
-
---
+++ lea.ino
@@ -0,0 +1,53 @@
+#define NBDIGITS 8
+#define NBDISPLAY 5
+
+#define A 2
+#define B 3
+#define C 4
+#define D 5
+#define E 6
+#define G 7
+#define H 8
+#define DP 9 // F
+
+const int nb_pins[5] = {A1, A2, A3, A4, A5};
+const int letter_pins[NBDIGITS] = {A, B, C, D, E, G, H, DP};
+
+void setup() {
+  for(int i = 0; i < NBDISPLAY; i++){
+    pinMode(nb_pins[i], OUTPUT);
+    digitalWrite(nb_pins[i], 1);
+  }
+  for(int i = 0; i < NBDIGITS; i++){
+    pinMode(letter_pins[i], OUTPUT);
+  }
+  
+  // Test first display
+  /*for(int i = 0; i < 10; i++){
+    setDigit(1, i);
+    delay(1000);
+  }*/
+}
+
+void loop() {
+  printNumber(12345);
+}
+
+// Get digit sequence to print a specific number
+void setArrayContent(bool* array, bool n1, bool n2, bool n3, bool n4, bool n5, bool n6, bool n7, bool n8){
+  bool sequence[NBDIGITS] = {n1,n2,n3,n4,n5,n6,n7,n8};
+  for (size_t i = 0; i < NBDIGITS; i++) {
+    *array = sequence[i];
+    array++;
+  }
+}
+
+// Get digit sequence to print a specific number
+void setSmallArrayContent(bool* array, bool n1, bool n2, bool n3, bool n4, bool n5){
+  bool sequence[NBDISPLAY] = {n1,n2,n3,n4,n5};
+  for (size_t i = 0; i < NBDISPLAY; i++) {
+    *array = sequence[i];
+    array++;
+  }
+}
+
